848d0160cc46073d2e3d992ca284cc5e3efc1ade
shaxruxpirimbetov <shaxruxpirimbetov@gmail.com>
2025-09-21 09:28:02 +0500

    update

--- www/js/config.js
+++ www/js/config.js
@@ -1,2 +1,2 @@
-//const baseUrl = "http://127.0.0.1:8000"
-const baseUrl = "https://shaxcoder.pythonanywhere.com"
+const baseUrl = "http://127.0.0.1:8000"
+// const baseUrl = "https://shaxcoder.pythonanywhere.com"
